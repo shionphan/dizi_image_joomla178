a262301e5f6f69a382bab68a203cfbb438607349
shionphan <shionphan@126.com>
2018-03-06 15:44:20 +0800

    1. Add watermark config for upload image. 2. fix Joomla 3.8.5 Image libraries bug.

--- admin/controllers/ajax.php
+++ admin/controllers/ajax.php
@@ -18,7 +18,7 @@ jimport( 'joomla.filesystem.file' );
 jimport( 'joomla.filesystem.folder' );
 
 //  image
-require_once JPATH_ROOT . '/libraries/joomla/image/image.php';
+require_once JPATH_ROOT . '/libraries/src/Image/Image.php';
 
 /**
  * ajax actions controller class.
@@ -77,7 +77,16 @@ class DiControllerAjax extends JControllerAdmin
 			$media_path 		= JPATH_ROOT . '/' . $media_component_params->get( 'image_path' ) . '/' . $di_directory;
 			$media_url 			= JUri::root() . '/' . $media_component_params->get( 'image_path' ) . '/' . $di_directory;
 			$quality 			= $di_component_params->get( 'quality', 80 );
-			
+
+			// Create param
+			$waterparam			= array();
+			$waterparam['watermarkfile'] = $di_component_params->get( 'watermarkfile', '' );
+			$waterparam['horiz_position'] = $di_component_params->get( 'horiz_position', 'center' );
+			$waterparam['horiz_shift'] = (int) $di_component_params->get( 'horiz_shift', 0 );
+			$waterparam['vert_position'] = $di_component_params->get( 'vert_position', 'bottom' );
+			$waterparam['vert_shift'] = (int) $di_component_params->get( 'vert_shift', 10 );
+			$waterparam['transparency'] = (int) $di_component_params->get( 'transparency', 50 );
+
 			//  get allowed image extensions
 			$image_extensions 	= $media_component_params->get( 'image_extensions' );
 			if( !empty( $image_extensions ) )
@@ -136,7 +145,8 @@ class DiControllerAjax extends JControllerAdmin
 								$size->height,
 								$quality,
 								null,
-								$size->crop
+								$size->crop,
+								$waterparam
 							);
 						}
 						
@@ -329,6 +339,15 @@ class DiControllerAjax extends JControllerAdmin
 			$media_path 		= JPATH_ROOT . '/' . $media_component_params->get( 'image_path' ) . '/' . $di_directory;
 			$media_url 			= JUri::root() . '/' . $media_component_params->get( 'image_path' ) . '/' . $di_directory;
 			$quality 			= (int) $di_component_params->get( 'quality', 80 );
+
+			// Create param
+			$waterparam			= array();
+			$waterparam['watermarkfile'] = $di_component_params->get( 'watermarkfile', '' );
+			$waterparam['horiz_position'] = $di_component_params->get( 'horiz_position', 'center' );
+			$waterparam['horiz_shift'] = (int) $di_component_params->get( 'horiz_shift', 0 );
+			$waterparam['vert_position'] = $di_component_params->get( 'vert_position', 'bottom' );
+			$waterparam['vert_shift'] = (int) $di_component_params->get( 'vert_shift', 10 );
+			$waterparam['transparency'] = (int) $di_component_params->get( 'transparency', 50 );
 			
 			$object_image_id = JRequest::getVar( 'object_image_id', null );
 			
@@ -382,7 +401,8 @@ class DiControllerAjax extends JControllerAdmin
 								$size->height,
 								$quality,
 								null,
-								$size->crop
+								$size->crop,
+								$waterparam
 							);
 							
 							if( file_exists( $filename ) )
@@ -587,7 +607,7 @@ class DiControllerAjax extends JControllerAdmin
 	 *  $format - image output format
 	 *  $crop - T/F to crop and center image or not
 	 */
-	function create_image( $source, $target, $width, $height, $quality = '80', $format = null, $crop = 0, $x = null, $y = null )
+	function create_image( $source, $target, $width, $height, $quality = '80', $format = null, $crop = 0, $waterparam, $x = null, $y = null )
 	{
 		$parts = explode( '.', $source );
 		$parts_count = count( $parts );
@@ -627,7 +647,139 @@ class DiControllerAjax extends JControllerAdmin
 					break;
 			}
 		
-		$image = new JImage( $source );
+		/**
+		 * Add watermark by @https://github.com/shionphan 2018-03-06
+		 * @param watermarkfile; Path to watermark file or URL
+		 * @param horiz_position; Watermark horizontal position;
+		 * @param horiz_shift; Left position of watermark. Use negative for right position. Leave empty for center.
+		 * @param vert_position; Watermark Vertical position;
+		 * @param vert_shift; Top position of watermark. Use negative for bottom position. Leave empty for center.
+		 * @param transparency; Transparency in percent. 0 = watermark invisible, 100 = not transparent (except for the set transparent color!). Ignored for true alpha blending.
+		 */
+		//Add Begin	
+		$watermarkfile = $waterparam['watermarkfile'];
+		$horiz_position = $waterparam['horiz_position'];
+		$horiz_shift = $waterparam['horiz_shift'];
+		$vert_position = $waterparam['vert_position'];
+		$vert_shift = $waterparam['vert_shift'];
+		$transparency = $waterparam['transparency'];
+		$transparency_type = 'alpha';
+		$transcolor = false;
+
+		static $disable_wm_ext_warning, $disable_wm_load_warning, $disable_alpha_warning;
+
+		if($transparency_type == 'alpha') {
+			$transcolor = FALSE;
+		}
+
+		$sourceType = strtolower(substr($source, strlen($source)-3));
+		switch($sourceType) {
+			case 'png':
+				$source_id = @imagecreatefrompng($source);
+				break;
+			case 'gif':
+				$source_id = @imagecreatefromgif($source);
+				break;
+			case 'jpg':
+				$source_id = @imagecreatefromjpeg($source);
+				break;
+			default:
+				$source = basename($source);
+				if(!$disable_wm_ext_warning) $this->multithumb_msg .= "You cannot use a .$fileType file ($watermarkfile) as a watermark<br />\\n";
+				$disable_wm_ext_warning = true;
+				return false;
+		}
+
+		//Get the resource ids of the pictures
+		$fileType = strtolower(substr($watermarkfile, strlen($watermarkfile)-3));
+		switch($fileType) {
+			case 'png':
+				$watermarkfile_id = @imagecreatefrompng($watermarkfile);
+				break;
+			case 'gif':
+				$watermarkfile_id = @imagecreatefromgif($watermarkfile);
+				break;
+			case 'jpg':
+				$watermarkfile_id = @imagecreatefromjpeg($watermarkfile);
+				break;
+			default:
+				$watermarkfile = basename($watermarkfile);
+				if(!$disable_wm_ext_warning) $this->multithumb_msg .= "You cannot use a .$fileType file ($watermarkfile) as a watermark<br />\\n";
+				$disable_wm_ext_warning = true;
+				return false;
+		}
+		if(!$watermarkfile_id) {
+			if(!$disable_wm_load_warning) $this->multithumb_msg .= "There was a problem loading the watermark $watermarkfile<br />\\n";
+			$disable_wm_load_warning = true;
+			return false;
+		}
+
+		@imageAlphaBlending($watermarkfile_id, false);
+		$result = @imageSaveAlpha($watermarkfile_id, true);
+		if(!$result) {
+			if(!$disable_alpha_warning) $this->multithumb_msg .= "Watermark problem: your server does not support alpha blending (requires GD 2.0.1+)<br />\\n";
+			$disable_alpha_warning = true;
+			imagedestroy($watermarkfile_id);
+			return false;
+		}
+
+		//Get the sizes of both pix
+		$sourcefile_width=imagesx($source_id);
+		$sourcefile_height=imagesy($source_id);
+		$watermarkfile_width=imagesx($watermarkfile_id);
+		$watermarkfile_height=imagesy($watermarkfile_id);
+
+		switch ($horiz_position) {
+			case 'center':
+				$dest_x = ( $sourcefile_width / 2 ) - ( $watermarkfile_width / 2 );
+				break;
+			case 'left':
+				$dest_x = $horiz_shift;
+				break;
+			case 'right':
+				$dest_x = $sourcefile_width - $watermarkfile_width + $horiz_shift;
+				break;
+		}
+
+		switch ($vert_position) {
+			case 'middle':
+				$dest_y = ( $sourcefile_height / 2 ) - ( $watermarkfile_height / 2 );
+				break;
+			case 'top':
+				$dest_y = $vert_shift;
+				break;
+			case 'bottom':
+				$dest_y = $sourcefile_height - $watermarkfile_height + $vert_shift;
+				break;
+		}
+
+		// if a gif, we have to upsample it to a truecolor image
+		if($fileType == 'gif') {
+			// create an empty truecolor container
+			$tempimage = imagecreatetruecolor($sourcefile_width, $sourcefile_height);
+
+			// copy the 8-bit gif into the truecolor image
+			imagecopy($tempimage, $source, 0, 0, 0, 0, $sourcefile_width, $sourcefile_height);
+
+			// copy the source_id int
+			$source_id = $tempimage;
+		}
+
+		if($transcolor!==false) {
+			imagecolortransparent($watermarkfile_id, $transcolor); // use transparent color
+			imagecopymerge($source_id, $watermarkfile_id, $dest_x, $dest_y, 0, 0, $watermarkfile_width, $watermarkfile_height, $transparency);
+		} else {
+			imagecopy($source_id, $watermarkfile_id, $dest_x, $dest_y, 0, 0, $watermarkfile_width, $watermarkfile_height); // True alphablend
+		}
+
+		imagedestroy($watermarkfile_id);
+		header("Content-type: image/jpeg");
+		imagejpeg($source_id,$target,100);
+		imagedestroy($source_id);
+		$image = new JImage($target);
+		//Add End;
+
+		//$image = new JImage( $source );
 		
 		if( !empty( $crop ) )
 			//$image->crop( $width, $height, $x, $y, false );
